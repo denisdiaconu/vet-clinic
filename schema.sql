96034ad22f5b804ed3c8c61e929f554348cca566
denisdiaconu <denisdiaconu117@yahoo.com>
2021-09-21 16:21:11 +0200

    Add data for new animals

--- schema.sql
+++ schema.sql
@@ -1,7 +1,7 @@
 /* Database schema to keep the structure of entire database. */
 
 CREATE TABLE animals (
-    id INT AUTO_INCREMENT,
+    id INT AUTO_INCREMENT PRIMARY KEY (id),
     name varchar(100),
     date_of_birth DATE,
     escape_attempts INT NOT NULL,
