ad9e423782c50ed167c44a4011fb7af5bb33aac9
denisdiaconu <denisdiaconu117@yahoo.com>
2021-09-21 16:07:47 +0200

    Add new column species

--- queries.sql
+++ queries.sql
@@ -9,3 +9,5 @@ SELECT name, escape_attempts FROM animals WHERE weight_kg > 10.5;
 SELECT * FROM animals WHERE neutered = true;
 SELECT * FROM animals WHERE name != 'Gabumon';
 SELECT * FROM animals WHERE weight_kg BETWEEN 10.4 AND 17.3;
+
+ALTER TABLE animals ADD COLUMN species varchar(100);
