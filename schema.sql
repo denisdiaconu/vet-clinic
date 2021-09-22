8fc4675c1b75ecaa0b208be3e0180b23e4c957b3
denisdiaconu <denisdiaconu117@yahoo.com>
2021-09-22 14:06:19 +0200

    Add two tables: owners, species

--- schema.sql
+++ schema.sql
@@ -1,12 +1,30 @@
 /* Database schema to keep the structure of entire database. */
 
 CREATE TABLE animals (
-    id INT AUTO_INCREMENT PRIMARY KEY (id),
+    id INT AUTO_INCREMENT,
     name varchar(100),
     date_of_birth DATE,
     escape_attempts INT NOT NULL,
     neutered BOOLEAN,
-    weight_kg DECIMAL NOT NULL
+    weight_kg DECIMAL NOT NULL,
+    PRIMARY KEY (id)
 );
 
 ALTER TABLE animals ADD COLUMN species varchar(100);
+
+
+/* Part 3 */
+
+
+CREATE TABLE owners (
+    id INT NOT NULL AUTO_INCREMENT,
+    full_name varchar(100),
+    age INT,
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE species (
+    id INT NOT NULL AUTO_INCREMENT,
+    name varchar(100),    
+    PRIMARY KEY(id)
+);
