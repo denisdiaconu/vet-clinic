14a232b42b6e2feba01b2260bcd3e1d4f83ed499
denisdiaconu <denisdiaconu117@yahoo.com>
2021-09-22 15:58:05 +0200

    Add all queries for tables

--- queries.sql
+++ queries.sql
@@ -45,3 +45,40 @@ SELECT * FROM animals WHERE neutered = true OR neutered = false GROUP BY neutere
 
 SELECT neutered, MIN(weight_kg), MAX(weight_kg) FROM animals GROUP BY neutered;
 SELECT AVG(escape_attempts), species FROM animals WHERE date_of_birth > '1990-01-01' AND date_of_birth < '2000-12-01' GROUP BY species;
+
+
+
+
+/* part 3 */
+
+
+SELECT * FROM animals
+    INNER JOIN owners
+    ON owner_id = owners.id WHERE full_name = 'Melody Pond';
+
+SELECT * FROM animals
+    INNER JOIN species
+    ON species.id = animals.species_id WHERE species.name = 'Pokemon';
+
+SELECT full_name, name FROM owners
+    LEFT JOIN animals
+    ON animals.owner_id = owners.id;
+
+SELECT species.name, COUNT(*) FROM animals
+    FULL OUTER JOIN species
+    ON species.id = animals.species_id GROUP BY species.id;
+
+SELECT animals.name, species.name FROM animals
+    INNER JOIN owners
+    ON owners.id = animals.owner_id
+    INNER JOIN species
+    ON species.id = animals.species_id
+    WHERE owners.full_name = 'Jennifer Orwell' AND species.name = 'Digimon';
+
+SELECT animals.name FROM animals
+    LEFT JOIN owners ON owners.id = animals.owner_id
+    WHERE animals.escape_attempts = 0 AND owners.full_name = 'Dean Winchester';
+
+SELECT owners.full_name, COUNT(animals.owner_id) FROM animals 
+    FULL OUTER JOIN owners
+    ON animals.owner_id = owners.id GROUP BY owners.id;
