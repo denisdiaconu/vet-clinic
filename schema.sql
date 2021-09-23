b6c921ca12991a2447e81dee6805fc119734182e
denisdiaconu <denisdiaconu117@yahoo.com>
2021-09-23 14:02:44 +0200

    Add tables specializations,visits and vets

--- schema.sql
+++ schema.sql
@@ -34,3 +34,34 @@ ALTER TABLE animals ADD species_id INT;
 ALTER TABLE animals ADD owner_id INT;
 ALTER TABLE animals ADD FOREIGN KEY (species_id) REFERENCES species(id);
 ALTER TABLE animals ADD FOREIGN KEY (owner_id) REFERENCES owners(id);
+
+
+/* Part 4 */
+
+
+CREATE TABLE vets (
+    id INT NOT NULL AUTO_INCREMENT,
+    name varchar(100),
+    age INT,
+    date_of_graduation DATE,
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE specializations (
+    species_id  INT,
+    vets_id     INT,
+    FOREIGN KEY (species_id) REFERENCES species (id),
+    FOREIGN KEY (vets_id) REFERENCES vets (id),
+    PRIMARY KEY (species_id, vets_id)
+);
+
+
+CREATE TABLE visits (
+    id INT NOT NULL AUTO_INCREMENT,
+    animals_id INT,
+    vets_id INT,
+    date_of_visit DATE,
+    FOREIGN KEY (animals_id) REFERENCES animals (id),
+    FOREIGN KEY (vets_id) REFERENCES vets (id),
+    PRIMARY KEY (id)
+);
